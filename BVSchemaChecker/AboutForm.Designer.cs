7a3a1b277432d540b95c4d0b014ad0d056984ed0
manderson88 <mark.anderson@villanova.edu>
2017-02-28 08:08:33 -0500

    Updated messages clean up for release

--- BVSchemaChecker/AboutForm.Designer.cs
+++ BVSchemaChecker/AboutForm.Designer.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿namespace BVSchemaChecker
 {
     partial class AboutForm
@@ -70,77 +69,4 @@
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Label lblAbout;
     }
-=======
-﻿namespace BVSchemaChecker
-{
-    partial class AboutForm
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.btnOK = new System.Windows.Forms.Button();
-            this.lblAbout = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // btnOK
-            // 
-            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(92, 178);
-            this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
-            this.btnOK.TabIndex = 0;
-            this.btnOK.Text = "OK";
-            this.btnOK.UseVisualStyleBackColor = true;
-            // 
-            // lblAbout
-            // 
-            this.lblAbout.AutoSize = true;
-            this.lblAbout.Location = new System.Drawing.Point(35, 62);
-            this.lblAbout.Name = "lblAbout";
-            this.lblAbout.Size = new System.Drawing.Size(189, 52);
-            this.lblAbout.TabIndex = 1;
-            // 
-            // AboutForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.lblAbout);
-            this.Controls.Add(this.btnOK);
-            this.Name = "AboutForm";
-            this.Text = "About BVSchemaChecker";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
-        }
-
-        #endregion
-
-        private System.Windows.Forms.Button btnOK;
-        private System.Windows.Forms.Label lblAbout;
-    }
->>>>>>> 9e8f63686db2e02b072ffbb4c674d5e668dc346c
 }
--- BVSchemaChecker/SchemaListForm.cs
+++ BVSchemaChecker/SchemaListForm.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿//---------------------------------------------------------------------------------------
 // $Workfile: SchemaListForm.cs $ : implementation of the form
 //
@@ -62,68 +61,3 @@ namespace BVSchemaChecker
         }
     }
 }
-=======
-﻿//---------------------------------------------------------------------------------------
-// $Workfile: SchemaListForm.cs $ : implementation of the form
-//
-// --------------------------------------------------------------------------------------
-// NOTICE TO ALL PERSONS HAVING ACCESS HERETO:  This document or recording contains 
-// computer software or related information constituting proprietary trade secrets of 
-// Black & Veatch Holding Company, which have been maintained in "unpublished" status 
-// under the copyright laws, and which are to be treated by all persons having access 
-// thereto in manner to preserve the status thereof as legally protectable trade secrets 
-// by neither using nor disclosing the same to others except as may be expressly 
-// authorized in advance by Black & Veatch Holding Company.  However, it is intended
-// that all prospective rights under the copyright laws in the event of future 
-// "publication" of this work shall also be reserved; for which purpose only, the 
-// following is included in this notice, to wit, 
-// "(C) COPYRIGHT 2016 BY BLACK & VEATCH HOLDING COMPANY, ALL RIGHTS RESERVED"
-//---------------------------------------------------------------------------------------
-/*
- * CHANGE LOG
- * $Archive: /MDL/BVSchemaChecker/SchemaListForm.cs $
- * $Revision: 1 $
- * $Modtime: 2/24/17 8:18a $
- * $History: SchemaListForm.cs $
- * 
- * *****************  Version 1  *****************
- * User: Mark.anderson Date: 2/24/17    Time: 9:25a
- * Created in $/MDL/BVSchemaChecker
- * Initial VSS commit of the schema checker source code.
- * 
- */
-//----------------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-
-namespace BVSchemaChecker
-{
-    /// <summary>
-    /// This is the form that displays the schema in the file.
-    /// </summary>
-    public partial class SchemaListForm :
-#if ADDIN
-      Bentley.MicroStation.WinForms.Adapter
-#else
-      Form
-#endif
-    {
-       
-        public SchemaListForm()
-        {
-            InitializeComponent();
-        }
-        public void SetSchemaNames(List<string> values)
-        {
-            foreach (string name in values)
-                lstBxSchemas.Items.Add(name);
-        }
-    }
-}
->>>>>>> 9e8f63686db2e02b072ffbb4c674d5e668dc346c
